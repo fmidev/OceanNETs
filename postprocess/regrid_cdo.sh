18f1b8504e7f577b00bc2a6e4bd465bc9620e2ab
Tommi Bergman <tommi.bergman@fmi.fi>
2024-02-26 15:27:55 +0200

    regrid to 1x1 with cdo

--- postprocess/regrid_cdo.sh
+++ postprocess/regrid_cdo.sh
@@ -4,7 +4,9 @@ exp='D30H'
 which basename
 basename
 
-for exp in 'D30H' 'D40H' 'C40H' 'E40H'
+#for exp in 
+#for exp in 'D30H' 'E40H'
+for exp in 'E40H'
 do
     DATAPATH='/Volumes/ONETs-SSD/'${exp}'/'
 
@@ -15,6 +17,13 @@ do
         ff="$(basename ${file})"
         outdata=${ff%.nc}.1x1.nc
         echo ${outdata}
-        cdo remapbil,mygrid $file ${DATAPATH}/1x1/${outdata} 
+        if [ ! -f "${DATAPATH}/1x1/${outdata}" ];then
+            if [[ ${file} = co2_AERmon.* ]]
+            then
+                cdo remapbil,mygrid -sellevel,1 $file ${DATAPATH}/1x1/${outdata}         
+            else
+                cdo remapbil,mygrid $file ${DATAPATH}/1x1/${outdata} 
+            fi
+        fi
     done  
 done
